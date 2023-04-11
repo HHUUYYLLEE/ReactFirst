1a5e86b691e0e7aac8d9145b730231f6ff011bba
HHUUYYLLEE <lebahuybk@gmail.com>
2023-04-11 22:07:13 +0700

    list

--- src/Components/TopBar/TopBar.jsx
+++ src/Components/TopBar/TopBar.jsx
@@ -4,8 +4,18 @@ import './TopBar.css'
 export default function topBar() {
   return (
     <div className='top'>
-      <div className='topLeft'>left</div>
-      <div className='topCenter'>center</div>
+      <div className='topLeft'>
+        <i class='fa-solid fa-gun'></i>
+      </div>
+      <div className='topCenter'>
+        <ul className='topList'>
+          <li className='topListItem'>Home</li>
+          <li className='topListItem'>About</li>
+          <li className='topListItem'>Contact</li>
+          <li className='topListItem'>Write</li>
+          <li className='topListItem'>Logout</li>
+        </ul>
+      </div>
       <div className='topRight'>right</div>
     </div>
   )
