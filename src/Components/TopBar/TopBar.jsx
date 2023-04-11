87dffd25461ecac0e90ddcbebe2491f73dae9d6b
HHUUYYLLEE <lebahuybk@gmail.com>
2023-04-11 22:19:34 +0700

    top bar 3

--- src/Components/TopBar/TopBar.jsx
+++ src/Components/TopBar/TopBar.jsx
@@ -5,7 +5,8 @@ export default function topBar() {
   return (
     <div className='top'>
       <div className='topLeft'>
-        <i class='fa-solid fa-gun'></i>
+        <i className='topIcon fa-brands fa-facebook'></i>
+        <i className='topIcon fa-brands fa-slack'></i>
       </div>
       <div className='topCenter'>
         <ul className='topList'>
@@ -16,7 +17,14 @@ export default function topBar() {
           <li className='topListItem'>Logout</li>
         </ul>
       </div>
-      <div className='topRight'>right</div>
+      <div className='topRight'>
+        <img
+          className='topImg'
+          src='https://archives.bulbagarden.net/media/upload/thumb/f/fb/0001Bulbasaur.png/180px-0001Bulbasaur.png'
+          alt=''
+        />
+        <i className='topSearchIcon fa-brands fa-searchengin'></i>
+      </div>
     </div>
   )
 }
