d23802fb9ed2f9b4ebd8104950167e9a0cb241fd
HHUUYYLLEE <lebahuybk@gmail.com>
2023-04-11 21:40:08 +0700

    top bar 2

--- src/Components/TopBar/TopBar.jsx
+++ src/Components/TopBar/TopBar.jsx
@@ -2,5 +2,11 @@ import React from 'react'
 import './TopBar.css'
 
 export default function topBar() {
-  return <div className='top'>topBar</div>
+  return (
+    <div className='top'>
+      <div className='topLeft'>left</div>
+      <div className='topCenter'>center</div>
+      <div className='topRight'>right</div>
+    </div>
+  )
 }
