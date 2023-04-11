9bc9989f83ecb8de63d6d761b43f1938186997a1
HHUUYYLLEE <lebahuybk@gmail.com>
2023-04-12 00:06:09 +0700

    upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import TopBar from './Components/TopBar/TopBar'
 import logo from './logo.svg'
 import BottomPart from './Components/Pages/BottomPart/BottomPart'
-
+import Write from './Components/Pages/Write/Write'
 function App() {
   return (
     <>
       <TopBar />
       <BottomPart />
+      <Write />
     </>
   )
 }
