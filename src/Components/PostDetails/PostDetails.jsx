9eec21bcaaf0e3b17d8128a5e0aa46395b95d584
HHUUYYLLEE <lebahuybk@gmail.com>
2023-04-11 23:50:39 +0700

    posts 4

--- src/Components/PostDetails/PostDetails.jsx
+++ src/Components/PostDetails/PostDetails.jsx
@@ -7,15 +7,25 @@ export default function PostDetails() {
         <img
           src='https://archives.bulbagarden.net/media/upload/thumb/5/55/Bag_Ultra_Ball_SV_Sprite.png/60px-Bag_Ultra_Ball_SV_Sprite.png'
           alt=''
-          className='singlePostImg'
+          className='postDetailsImg'
         />
-        <h1 className='singlePostTitle'>
+        <h1 className='postDetailsTitle'>
           Post Title
           <div className='postDetailsEdit'>
             <i className='postDetailsIcon fa-regular fa-pen-to-square'></i>
             <i className='postDetailsIcon fa-regular fa-trash-can'></i>{' '}
           </div>
         </h1>
+        <div className='postDetailsInfo'>
+          <span className='postDetailsAuthor'>
+            Author: <b>Huy</b>
+          </span>
+          <span className='postDetailsDate'>An hour ago</span>
+        </div>
+        <p className='postDetailsDescription'>
+          Post DescriptionPost DescriptionPost DescriptionPost DescriptionPost DescriptionPost DescriptionPost
+          DescriptionPost DescriptionPost DescriptionPost Description
+        </p>
       </div>
     </div>
   )
